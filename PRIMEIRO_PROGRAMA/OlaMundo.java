49e8524e40ad4f91349a930fcf0d6c54aacea2be
laeciodev <laeciojesusn@gmail.com>
2025-01-06 21:44:22 -0300

    Primeiro código em Java

---
+++ PRIMEIRO_PROGRAMA/OlaMundo.java
@@ -0,0 +1,13 @@
+package PRIMEIRO_PROGRAMA;
+
+public class OlaMundo {
+    
+    public static void main(String[] args){
+        // 1- Primeiro programa
+        System.out.println("Meu primeiro programa em Java");
+
+        System.out.println("A versão do Java é: " + System.getProperty("java.version"));
+
+        //Código
+    }
+}
